6d64b9074ba54d8f8fb64c8db6dd5d3de0135681
hsuyee <hsu.yee.htike@gmail.com>
2014-08-12 21:13:44 -0700

    project test

---
+++ Assignment2/Candidate.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Candidate.h
+ * Author: kadaj
+ *
+ * Created on August 13, 2014, 12:22 AM
+ */
+
+#ifndef CANDIDATE_H
+#define	CANDIDATE_H
+
+
+
+#endif	/* CANDIDATE_H */
+
---
+++ Assignment2/Test.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Test.h
+ * Author: kadaj
+ *
+ * Created on August 13, 2014, 12:25 AM
+ */
+
+#ifndef TEST_H
+#define	TEST_H
+
+
+
+#endif	/* TEST_H */
+

a1ab17b30cb0097ee177d1cfd0578705b9e858ac
hsuyee <hsuyee@ubuntu>
2014-08-13 15:14:14 +0800

    Commit from ubuntu 64

--- TMS/Main.cpp
+++ TMS/Main.cpp
@@ -31,6 +31,7 @@ void Menu()
     cout << "3) Exit \n";
     cout << "4) Error error \n";
     cout << "Select a choice: ";
+    cout << "MAKE NEW CHANGES..."
     cin >> select;
     system("clear");
     
